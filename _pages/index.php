e50abb526a7af817ffa7632dd29338194cb1feef
Karsten Horsmann <khorsmann@gmail.com>
2019-11-08 11:50:13 +0100

    Delete index.php

--- _pages/index.php
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<meta charset="utf-8">
-<title>Redirecting to https://lug-hannover.de</title>
-<meta http-equiv="refresh" content="0; URL=https://lug-hannover.de">
-<link rel="canonical" href="https://lug-hannover.de">
